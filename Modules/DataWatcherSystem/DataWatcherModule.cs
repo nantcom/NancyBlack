504c6b17c93105764442f8c06bc3396bfcd2ed8a
Jirawat Padungkijjanont <me@nant.co>
2015-07-02 05:12:13 +0700

    - ADD: #2831 Log Table, called: "RowVersion"

--- Modules/DataWatcherSystem/DataWatcherModule.cs
+++ Modules/DataWatcherSystem/DataWatcherModule.cs
@@ -2,18 +2,34 @@
 using Nancy.Bootstrapper;
 using NantCom.NancyBlack.Configuration;
 using NantCom.NancyBlack.Modules.DatabaseSystem;
+using NantCom.NancyBlack.Modules.MembershipSystem;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace NantCom.NancyBlack.Modules
 {
     public class DataWatcherModule : BaseModule, IPipelineHook
     {
+        private class DatabaseEvent
+        {
+            public string Action { get; set; }
+
+            public NancyBlackDatabase Database { get; set; }
+
+            public string DataTypeName { get; set; }
+
+            public dynamic AffectedRow { get; set; }
+        }
+
+        private static ConcurrentBag<DatabaseEvent> _Events = new ConcurrentBag<DatabaseEvent>();
+
         static DataWatcherModule()
         {
             NancyBlackDatabase.ObjectCreated += NancyBlackDatabase_ObjectCreated;
@@ -21,6 +37,54 @@ namespace NantCom.NancyBlack.Modules
             NancyBlackDatabase.ObjectUpdated += NancyBlackDatabase_ObjectUpdated;
         }
 
+        private static void NancyBlackDatabase_ObjectUpdated(NancyBlackDatabase arg1, string arg2, dynamic arg3)
+        {
+            if (arg2 == "rowversion")
+            {
+                return;
+            }
+
+            _Events.Add(new DatabaseEvent()
+            {
+                Action = "update",
+                AffectedRow = (object)arg3,
+                Database = arg1,
+                DataTypeName = arg2
+            });
+        }
+
+        private static void NancyBlackDatabase_ObjectDeleted(NancyBlackDatabase arg1, string arg2, dynamic arg3)
+        {
+            if (arg2 == "rowversion")
+            {
+                return;
+            }
+
+            _Events.Add(new DatabaseEvent()
+            {
+                Action = "delete",
+                AffectedRow = (object)arg3,
+                Database = arg1,
+                DataTypeName = arg2
+            });
+        }
+
+        private static void NancyBlackDatabase_ObjectCreated(NancyBlackDatabase arg1, string arg2, dynamic arg3)
+        {
+            if (arg2 == "rowversion")
+            {
+                return;
+            }
+
+            _Events.Add(new DatabaseEvent()
+            {
+                Action = "created",
+                AffectedRow = (object)arg3,
+                Database = arg1,
+                DataTypeName = arg2
+            });
+        }
+        
         public DataWatcherModule()
         {
             Get["/Admin/DataWatcher"] = this.HandleStaticRequest("admin-datawatcher", null);
@@ -39,45 +103,35 @@ namespace NantCom.NancyBlack.Modules
             {
             }
         }
-
-        private static void NancyBlackDatabase_ObjectUpdated(NancyBlackDatabase arg1, string arg2, dynamic arg3)
-        {
-        }
-
-        private static void NancyBlackDatabase_ObjectDeleted(NancyBlackDatabase arg1, string arg2, dynamic arg3)
-        {
-        }
         
-        private static void NancyBlackDatabase_ObjectCreated(NancyBlackDatabase db, string entityName, dynamic value)
-        {
-        }
-        
-        /// <summary>
-        /// Logs the action on database
-        /// </summary>
-        /// <param name="dt"></param>
-        /// <param name="inputObject"></param>
-        private static void Log( string entityName, string action, int id, object inputObject, int byUserId)
-        {
-            //_db.Insert(new RowVersion()
-            //{
-            //    UserId = byUserId,
-            //    Action = action,
-            //    js_Row = JsonConvert.SerializeObject(inputObject),
-            //    RowId = id,
-            //    DataType = dt.NormalizedName,
-            //    __createdAt = DateTime.Now,
-            //    __updatedAt = DateTime.Now,
-            //    __version = DateTime.Now.Ticks.ToString(),
-            //});
-        }
-
         public void Hook(IPipelines p)
         {
             p.AfterRequest.AddItemToEndOfPipeline((ctx) =>
             {
-                // process log
-                string s = "";
+                var db = ctx.Items["SiteDatabase"];
+                var events = _Events.ToList();
+                var user = ctx.CurrentUser as NancyBlackUser;
+
+                _Events = new ConcurrentBag<DatabaseEvent>();
+
+                Task.Run(() =>
+                {
+                    foreach (var item in events)
+                    {
+                        item.Database.UpsertStaticRecord("rowversion", new RowVersion()
+                        {
+                            Action = item.Action,
+                            js_Row = JsonConvert.SerializeObject( item.AffectedRow ),
+                            UserId = user.Id,
+                            __createdAt = DateTime.Now,
+                            RowId = (int)item.AffectedRow.Id,
+                            DataType = item.DataTypeName
+                        });
+
+                        // send email
+                    }
+                });
+
             });
         }
     }
