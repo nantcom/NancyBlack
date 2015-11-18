79a7fb6eef3f5b1a8cc2ad6097924dd5df02db74
YoCom <yovich@nant.co>
2015-11-18 11:50:54 +0700

    Add: function for TrucateWithEllipsis

---
+++ General/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NantCom.NancyBlack.General
+{
+    public static class Extensions
+    {
+        public static string TrucateWithEllipsis(this string input, int maxLength)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input.Length <= maxLength ? input : input.Substring(0, maxLength) + "...";
+        }
+    }
+}
