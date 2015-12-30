f3592cbaa0485a25e759db5d2c46e9a15473c99c
YoCom <yovich@nant.co>
2015-12-30 19:11:16 +0700

    - forgot to add file ( the file I forgot to add are supports system file ) - Done: admin can update sale order status in support page - Done: admin can update sale order steps in support page (and can update image)

---
+++ Modules/CommerceSystem/SupportModule.cs
@@ -0,0 +1,34 @@
+﻿using NantCom.NancyBlack.Modules.CommerceSystem.types;
+using NantCom.NancyBlack.Modules.ContentSystem.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace NantCom.NancyBlack.Modules.CommerceSystem
+{
+    public class SupportModule : BaseModule
+    {
+        public SupportModule()
+        {
+            Get["/support/{saleOrderIdentifier}"] = this.HandleViewRequest("commerce-support", (arg) =>
+            {
+                var id = (string)arg.saleOrderIdentifier;
+                var so = this.SiteDatabase.Query<SaleOrder>()
+                            .Where(row => row.SaleOrderIdentifier == id)
+                            .FirstOrDefault();
+
+                var statusList = (typeof(SaleOrderStatus)
+                            .GetFields(BindingFlags.Public | BindingFlags.Static)
+                            .Where(f => f.FieldType == typeof(string)).Select(f => (string)f.GetValue(null))).ToList();
+
+                var dummyPage = new Page();
+
+                var data = new { StatusList = statusList, SaleOrder = so };
+
+                return new StandardModel(this, dummyPage, data);
+            });
+        }
+    }
+}
