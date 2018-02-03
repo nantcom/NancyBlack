c7e8cf3d328362746a35090937cab2b841fa0ed9
YoCom <tananut@nant.co>
2018-02-03 19:39:22 +0700

    Initial Commit of Merging Affiliate into SaleOrderDetail

---
+++ Modules/LogisticsSystem/Types/IShipmentTrackable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NantCom.NancyBlack.NancyBlack.Modules.LogisticsSystem.Types
+{
+    public interface IShipmentTrackable
+    {
+        /// <summary>
+        /// Date which reward has been ship out
+        /// </summary>
+        DateTime ShipOutDate { get; set; }
+
+        /// <summary>
+        /// id of logistics company which handle shipment
+        /// </summary>
+        int ShipByLogisticsCompanyId { get; set; }
+
+        string TrackingCode { get; set; }
+
+        string BookingCode { get; set; }
+    }
+}
