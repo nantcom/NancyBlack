259c660b9814285957cff72c640ffc9800aab7c1
YoCom <tananut@nant.co>
2018-02-06 18:52:59 +0700

    - successful in separate affiliate rewards claim from saleorderdetailmanager.cshtml - create memberprofile-admin.cshtml (need to add Profile and Purchase history in later version)

--- Modules/CommerceSystem/Views/Admin/js/saleorderdetail.js
+++ Modules/CommerceSystem/Views/Admin/js/saleorderdetail.js
@@ -268,22 +268,4 @@
         }
 
     });
-    
-    app.controller('AffilicateRewardsController', function ($scope, $http) {
-
-        var me = this;
-
-        $scope.rewardsClaims = window.allData.AffiliateRewardsClaims;
-        $scope.logisticsCompanies = window.allData.LogisticsCompanies;
-
-        me.openTrackingUrl = function (claim) {
-            var logisticsCompanies = $.grep($scope.logisticsCompanies, function (e) { return e.Id == claim.ShipByLogisticsCompanyId; });
-            if (logisticsCompanies.length == 0) {
-                alert('selected logistics company is not found.');
-            }
-            else {
-                window.open(logisticsCompanies[0].TrackingUrlFormat.replace("{0}", claim.TrackingCode));
-            }
-        }
-    });
 })();
