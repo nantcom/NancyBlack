d27d2bce6c912d9310f9b87a064b82f2e76b5a3c
YoCom <yovich@nant.co>
2018-09-19 17:48:03 +0700

    - add api /Admin/Member/Readjust/SaleOrder for add ncbUserId to SaleOrder (help when search saleorder filter by customer) - add variable readyToSendMail which use for temperately disable auto send email (set to false for disable auto send email)

--- Modules/CommerceSystem/CommerceModule.cs
+++ Modules/CommerceSystem/CommerceModule.cs
@@ -258,7 +258,6 @@ namespace NantCom.NancyBlack.Modules.CommerceSystem
 
             var saleorder = ((JObject)arg.body.Value).ToObject<SaleOrder>();
 
-            saleorder.NcbUserId = this.CurrentUser.Id;
             saleorder.PaymentStatus = PaymentStatus.WaitingForPayment;
             saleorder.Customer = this.CurrentUser.Profile;
             saleorder.AffiliateCode = this.Request.Cookies["source"];
