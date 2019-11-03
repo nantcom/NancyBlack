6eaa068ca0c86a5f673bc6040c95316cd07cafc5
YoKurz <tananut@nant.co>
2019-11-03 17:31:48 +0700

    - create searchserialmanager.cshtml, can be open in menu Sales of admin page

---
+++ Modules/CommerceSystem/Views/Admin/js/search-serial-manager.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Page', [])
+        .controller('PageController', PageController);
+
+    PageController.$inject = ['$location', '$window', '$scope', '$http', '$log'];
+
+    function PageController($location, $window, $scope, $http, $log) {
+        /* jshint validthis:true */
+        var vm = this;
+
+        $scope.results = [];
+
+        vm.searchBySerial = function () {
+
+            $http.get('/admin/search/by/serial?key=' + $scope.keyword, { })
+                .then(function (response) {
+                    $scope.results = response.data;
+                }, function (error) {
+                    alert(error.data.Message);
+                });
+        };
+        
+    }
+})();
