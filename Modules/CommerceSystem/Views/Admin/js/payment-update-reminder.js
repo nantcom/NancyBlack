262cd653c7bb8907450ff84aacb3c258f98ec408
YoKurz <tananut@nant.co>
2019-12-27 13:19:08 +0700

    -create Payment Update Reminder system

---
+++ Modules/CommerceSystem/Views/Admin/js/payment-update-reminder.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Page', [])
+        .filter('PurStatusFilter', PurStatusFilter)
+        .controller('PageController', PageController);
+
+    PageController.$inject = ['$location', '$window', '$scope', '$http', '$log', '$filter'];
+
+    function PageController($location, $window, $scope, $http, $log) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.updateStatus = function (pur, statusId) {
+
+            $http.post('/admin/pur/statusupdate', { purId: pur.Id, status: statusId })
+                .then(function (response) {
+                    pur = response.data;
+                    alert("updated");
+                }, function (error) {
+                    alert(error.data.Message);
+                });
+        };
+
+        //$filter('filtername');
+
+        $scope.purStatuses = window.purStatuses;
+        $scope.object = {};
+        $scope.object.purStatuses = $scope.purStatuses;
+    }
+
+    function PurStatusFilter() {
+
+        return function (input) {
+
+            for (var i = 0; i < window.purStatuses.length; i++) {
+                if (window.purStatuses[i].Item1 === input) {
+                    return window.purStatuses[i].Item2;
+                }
+            }
+
+            return 'not found';
+        };
+    }
+})();
---
+++ Modules/CommerceSystem/types/PaymentUpdateReminder.cs
@@ -0,0 +1,60 @@
+﻿using NantCom.NancyBlack.Modules.DatabaseSystem.Types;
+using Newtonsoft.Json.Linq;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NantCom.NancyBlack.Modules.CommerceSystem.types
+{
+    public enum PaymentUpdateReminderStatus
+    {
+        AutoAccepted = 0,
+        Accepted = 1,
+        Rejected = 2,
+        Pending = 3,
+
+        //FollowUp means there is remaining amount to be paid
+        FollowUp = 4
+    }
+
+    public class PaymentUpdateReminder : IStaticType
+    {
+        public int Id { get; set; }
+
+        public DateTime __createdAt { get; set; }
+
+        public DateTime __updatedAt { get; set; }
+
+        public int SaleOrderId { get; set; }
+
+        public PaymentUpdateReminderStatus Status { get; private set; }
+
+        public string Note { get; set; }
+
+        public int HashAttachmentsText { get; private set; }
+
+        public bool HasAttachmentBeenUpdated { get; private set; }
+
+        public void UpdateHasAttachmentsString(SaleOrder so)
+        {
+            int currentHash = so.Attachments.GetHashCode();
+
+            // do nothing if there is no update
+            if (this.HashAttachmentsText != currentHash)
+            {
+                this.HasAttachmentBeenUpdated = true;
+                this.HashAttachmentsText = currentHash;
+                this.Status = PaymentUpdateReminderStatus.Pending;
+            }
+        }
+
+        public void SetStatus(SaleOrder so, PaymentUpdateReminderStatus status)
+        {
+            this.HasAttachmentBeenUpdated = status == PaymentUpdateReminderStatus.Pending;
+            this.Status = status;
+            this.HashAttachmentsText = so.Attachments.GetHashCode();
+        }
+    }
+}
